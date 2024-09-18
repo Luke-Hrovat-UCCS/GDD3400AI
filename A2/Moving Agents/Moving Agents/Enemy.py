4a810d716f5f0c70408ec8bffd7e9adbd4cc03af
Luke Hrovat <lhrovat@uccs.edu>
2024-09-18 13:07:55 -0600

    finished a2

--- A2/Moving Agents/Moving Agents/Enemy.py
+++ A2/Moving Agents/Moving Agents/Enemy.py
@@ -35,7 +35,7 @@ class Enemy(Agent):
         lend = (self.center[0] + self.__velo.scale(self.size).a, self.center[1]+self.__velo.scale(self.size).b)
         pygame.draw.line(screen, (0,0,255), lstart, lend,3)
         if self.target is not None:
-            pygame.draw.line(screen, (255,0,0), self.center, self.target.center,3)  
+            pygame.draw.line(screen, (255,0,0), self.center, self.target.center,3)
         
     
     #updates position of enemy, Runs from player if player is in range, otherwise wanders, accepts a player object
@@ -46,23 +46,35 @@ class Enemy(Agent):
             return
         #if player is in range RUN AWAY
         #calculate vector from player to enemy
-        playerDist = Vector.Vector(player.center[0],player.center[1])
-        playerDist.__add__(Vector.Vector(self.center[0],self.center[1]))
+        playerDist = Vector.Vector(self.center[0],self.center[1])
+        playerDist.__sub__(Vector.Vector(player.center[0],player.center[1]))
 
+        d= ((player.center[0]-self.center[0])**2+(player.center[1]-self.center[1])**2)**.5
         #if length of that vector is less than the aggro range of the enemy, run away
-        if(playerDist.length() < Constants.AGGRO_RANGE):
+        if(d < Constants.AGGRO_RANGE):
             #set velocity to the player vector and normalize    
             self.__velo = playerDist.normalize()   
-            
+            self.target = player
+            #change direction if near the edge
+            if (self.__pos.a + self.__velo.a < 0 or self.__pos.a + self.__velo.a > Constants.WORLD_WIDTH):
+                self.__velo.a = -self.__velo.a
+            if (self.__pos.b + self.__velo.b < 0 or self.__pos.b + self.__velo.b > Constants.WORLD_HEIGHT):
+                self.__velo.b = -self.__velo.b
             #update position
             self.__pos.__add__(self.__velo.scale(Constants.ENEMY_SPEED))
-            self.target = player
+            
         else:
+            self.target = None
             #randomly wander a little bit    
             self.__velo.a +=  (random.randrange(-Constants.ENEMY_WANDER_RANGE,Constants.ENEMY_WANDER_RANGE )/Constants.FRAME_RATE)  
             self.__velo.b +=  (random.randrange(-Constants.ENEMY_WANDER_RANGE,Constants.ENEMY_WANDER_RANGE )/Constants.FRAME_RATE)
             self.__velo = self.__velo.normalize()
 
+            #change direction if near the edge
+            if (self.__pos.a + self.__velo.a < 0 or self.__pos.a + self.__velo.a > Constants.WORLD_WIDTH):
+                self.__velo.a = -self.__velo.a
+            if (self.__pos.b + self.__velo.b < 0 or self.__pos.b + self.__velo.b > Constants.WORLD_HEIGHT):
+                self.__velo.b = -self.__velo.b
             #update position
             self.__pos.__add__(self.__velo.scale(Constants.ENEMY_SPEED))
         #update center
